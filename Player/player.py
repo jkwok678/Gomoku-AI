076111a635595580e4376ee41fe96ff79427ba40
jkwok678 <jkwok678@gmail.com>
2021-03-03 20:41:17 +0000

    Changed the player minmax function

--- Player/player.py
+++ Player/player.py
@@ -16,23 +16,16 @@ class Player(GomokuAgent):
         self.X_IN_A_LINE = X_IN_A_LINE
 
     def minimax(self, target_depth, board):
+        # first move at the centre
         if(self.moves==0):
             print(self.BOARD_SIZE/2)
             return(int(self.BOARD_SIZE/2),int(self.BOARD_SIZE/2))
         else:
-            moves =[]
-            moves_score = []
-            print("start")
-            for row in range(self.BOARD_SIZE):
-                for single in range(self.BOARD_SIZE):
-                    if (board[row][single] == 0):
-                        new_move = np.copy(board)
-                        new_move[row][single] = 2
-                        moves.append(new_move)
-                        new_move_score = yourFunction(board)
-                        moves_score.append(new_move_score)
+            moves,moves_score = generateNewMoves(board)
+            #recursive function to generate moves and scores?
+            recursiveMiniMax(board, target_depth, currentDepth)
 
-            
+            for x in moves:
             max_score_index = max_score.index(max(moves_score))        
             move_to_take = moves[max_score_index]
             x_pos = 0
@@ -45,6 +38,27 @@ class Player(GomokuAgent):
 
             print("end")
             return(x_pos,x_pos)
+
+    def generateNewMoves(board):
+        moves =[]
+        moves_score =[]
+        for row in range(self.BOARD_SIZE):
+            for single in range(self.BOARD_SIZE):
+                if (board[row][single] == 0):
+                    new_move = np.copy(board)
+                    new_move[row][single] = 2
+                    moves.append(new_move)
+                    # Place your evaluation function here
+                    #new_move_score = yourFunction(board)
+                    moves_score.append(new_move_score)
+        return moves, moves_score
+
+    def recursiveMiniMax(board, target_depth, currentDepth):
+        moves,moves_score = generateNewMoves(board)
+        target_depth
+        for move in moves:
+            generateNewMoves
+
 
     def move(self, board):
         print(board)
@@ -59,12 +73,16 @@ class Player(GomokuAgent):
     #def calculate_score(self, board):
 
 
-"""class Node:
+class Node:
     self.parent;
     self.children = [];
-    self.score;
+    self.state;
+    self.score =0;
 
-    def __init__(self, parent):
+    def __init__(self, parent, state, score):
         self.parent = parent
-"""
+        self.state = state
+        self.score = score
+    
+
 
