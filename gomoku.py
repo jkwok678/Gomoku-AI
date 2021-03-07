4f13d5d5867f53786c3f5e123ca2df76e45a2d76
jkwok678 <jkwok678@gmail.com>
2021-03-07 12:41:32 +0000

    Gomoku game changed back to 11X11 and 5 in a row

--- gomoku.py
+++ gomoku.py
@@ -18,8 +18,8 @@ from random import randint
 
 from misc import winningTest, legalMove
 
-BOARD_SIZE = 4   # size of the board is 11-by-11
-X_IN_A_LINE = 3   # play the standard game with 5 stones in a line
+BOARD_SIZE = 11   # size of the board is 11-by-11
+X_IN_A_LINE = 5   # play the standard game with 5 stones in a line
 TIME_OUT = 5      # player must return a move within 5 seconds
 
 # an empty class to host the time-out exception
